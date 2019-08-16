cdba400b2593fc4018ac3997b34993cae3c8c9f3
yangzhifang <yangzhifang@910app.com>
2019-08-16 15:37:53 +0800

    增加

--- bin/f-web-cli.js
+++ bin/f-web-cli.js
@@ -4,23 +4,53 @@ const program = require('commander');
 const download = require('download-git-repo');
 const chalk = require('chalk');
 const ora = require('ora');
+const pkg = require('../package.json');
 
 
 program
-  .version('0.1.5')
-  .option('-i, init [name]', '初始化f-cli项目')
+  .version(pkg.version)
+  .option('-i, init [name]', '初始化web项目')  
+  .option('-m, initmini [name]', '初始化微信小程序项目')  
+  .option('-v, verbose', '打印详细日志')
   .parse(process.argv);
 
 
 if (program.init) {
-  const spinner = ora('正在从github下载f-web-cli').start();
+  const spinner = ora('downloading web...').start();
   download('frontyang/gulp-demo', program.init, function (err) {
     if (!err) {
       // 可以输出一些项目成功的信息
-      console.info(chalk.blueBright('下载成功'));
+      console.info(chalk.blueBright('Success!'));
     } else {
       // 可以输出一些项目失败的信息
-      console.info(err);
+      console.info(chalk.redBright('Error!'));
     }
   })
 }
+if (program.initmini) {
+  const spinner = ora('downloading miniprogram...').start();
+  download('frontyang/wechat-package', program.initmini, function (err) {
+    if (!err) {
+      // 可以输出一些项目成功的信息
+      console.info(chalk.blueBright('Success!'));
+    } else {
+      // 可以输出一些项目失败的信息
+      console.info(chalk.redBright('Error!'));
+    }
+  })
+}
+
+var verbose = false
+if (program.verbose) {
+  verbose = program.verbose
+}
+
+
+var _verbose = verbose
+function log(str) {
+  if (_verbose == true) {
+    console.log(str)
+  }
+}
+
+log('verbose = ' + verbose)
